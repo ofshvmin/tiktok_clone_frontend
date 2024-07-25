e374806cd6728fe4578cf39097506bc41683a02c
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-07-25 13:12:39 -0400

    stub up pages and stores

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import Feed from './pages/Feed.tsx'
+import Upload from './pages/Upload.tsx'
+import Profile from './pages/Profile.tsx'
+import Post from './pages/Post.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Feed />,
+  },
+  {
+    path: "/upload",
+    element: <Upload />,
+  },
+  {
+    path: "/profile/:id",
+    element: <Profile />,
+  },
+  {
+    path: "/post/:id",
+    element: <Post />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
 )
